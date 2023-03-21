d73ce106ff7ba4f2fb152724e507752f162a4fe5
Joaquin Rojas <quinjoa27@gmail.com>
2023-03-20 21:08:06 -0300

    Path * added to index

--- index.js
+++ index.js
@@ -21,6 +21,10 @@ app.use(express.static("public"));
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/events", require("./routes/events"));
 
+app.get("*", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
 // Listen petitions
 app.listen(process.env.PORT, () => {
   console.log(`Server running on port ${process.env.PORT}`);
